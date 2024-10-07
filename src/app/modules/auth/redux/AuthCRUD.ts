b3ef67bb3d4516e628fd21580e86cb1c57d3f6cb
Bryan Guerra <bryanmlg2018@gmail.com>
2024-10-07 13:17:10 -0600

    integrated public reset password fix

--- src/app/modules/auth/redux/AuthCRUD.ts
+++ src/app/modules/auth/redux/AuthCRUD.ts
@@ -87,7 +87,7 @@ export const updateUserPassword = async (password: string): Promise<void | strin
 
 export const requestPassword = async (email: string) => {
   const { error } = await supabase.auth.resetPasswordForEmail(email, {
-    redirectTo: 'https://feel-good-app.vercel.app/reset-password', // URL for reset page
+    redirectTo: 'https://feel-good-app.vercel.app/auth/reset-password', // URL for reset page
   })
 
   if (error) {
